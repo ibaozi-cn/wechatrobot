0cc5a743d8bd50749154d46b94a7de0078f6c9c0
zhangzhanyong <zhangzhanyong@julive.com>
2018-11-13 17:33:26 +0800

    过滤掉ai小哆

--- src/on-message.js
+++ src/on-message.js
@@ -7,24 +7,22 @@ const {
     Message,
 } = require('wechaty');
 
-async function onMessage(userKey,msg) {
+async function onMessage(userKey, msg) {
+
     console.log(` ${userKey} Message: ${msg}`);
 
     // Skip message from self, or inside a room
-    if (msg.self() || msg.room() || msg.from().name() === '微信团队' || msg.type() !== Message.Type.Text) return;
+    if (msg.self() || msg.room() || msg.from().name() === '微信团队' || msg.text() === 'Ai小哆' || msg.type() !== Message.Type.Text) return;
 
-    console.log('Bot', `talk: %s`, msg.text());
+    let text = msg.text();
 
     try {
         const {text: reply} = await tuling.ask(msg.text(), {userid: msg.from()});
-        console.log('校长', `reply:"%s" for "%s" `,
-            reply,
-            msg.text(),
-        );
         await msg.say(reply)
     } catch (e) {
         console.error('Bot', 'on message tuling.ask() exception: %s', e && e.message || e)
     }
+
 }
 
 module.exports = {
